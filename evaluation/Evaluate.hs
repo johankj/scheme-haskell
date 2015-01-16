6d7f7c0e8bd24f9bc4571c96211ed57047b476f2
Johan K. Jensen <jj@johanjensen.dk>
2015-01-16 22:32:11 +0100

    Chapter 5, Exercise 2: Add support for recursive equivalence checking for lists.

--- evaluation/Evaluate.hs
+++ evaluation/Evaluate.hs
@@ -194,15 +194,22 @@ eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
 eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
 eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
 eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
-eqv [(List arg1), (List arg2)]             =
-    return $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
-      where eqvPair (x1, x2) = case eqv [x1, x2] of
-                               Left err -> False
-                               Right (Bool val) -> val
+eqv [(List arg1), (List arg2)]             = eqvList eqv [(List arg1), (List arg2)]
 eqv [_, _]                                 = return $ Bool False
 eqv badArgList                             = throwError $ NumArgs 2 badArgList
 
+-- Recursively check equivalence of lists by function-argument
+eqvList :: ([LispVal] -> ThrowsError LispVal) -> [LispVal] -> ThrowsError LispVal
+eqvList eqvFunc [(List arg1), (List arg2)] =
+    return $ Bool $ (length arg1 == length arg2) &&
+                    (all eqvPair $ zip arg1 arg2)
+      where eqvPair (x1, x2) = case eqvFunc [x1, x2] of
+                                    Left err -> False
+                                    Right (Bool val) -> val
+
 equal :: [LispVal] -> ThrowsError LispVal
+equal [(List arg1), (List arg2)] = eqvList equal [(List arg1), (List arg2)]
+equal [(DottedList xs x), (DottedList ys y)] = equal [List $ xs ++ [x], List $ ys ++ [y]]
 equal [arg1, arg2] = do
     primitiveEquals <- liftM or $ mapM (unpackEquals arg1 arg2)
                        [AnyUnpacker unpackNumber, AnyUnpacker unpackString, AnyUnpacker unpackBoolean]
