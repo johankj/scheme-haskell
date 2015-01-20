201424c30a8a9f1f2e895ba4eb105737e5f33b7e
Johan K. Jensen <jj@johanjensen.dk>
2015-01-20 22:13:44 +0100

    Added REPL

--- evaluation/Evaluate.hs
+++ evaluation/Evaluate.hs
@@ -10,12 +10,40 @@ module Evaluate where
 import Parsing
 import System.Environment
 import Control.Monad.Error
+import System.IO
 
 main :: IO ()
 main = do
     args <- getArgs
-    evaled <- return $ liftM show $ (readExpr (head args) >>= eval)
-    putStrLn $ extractValue $ trapError evaled
+    case length args of
+      0 -> runRepl
+      1 -> evalAndPrint $ args !! 0
+      _ -> putStrLn "Program takes 0 or 1 arguments"
+
+-- REPL
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr =  evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+   result <- prompt
+   if pred result
+      then return ()
+      else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
+
 
 data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
 
