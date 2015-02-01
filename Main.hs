1ad0ca77dc9cbe3876de4fa2fb431fd176044691
Johan K. Jensen <jj@johanjensen.dk>
2015-02-01 09:48:29 +0100

    Update Main.hs to pass args along to the program

--- Main.hs
+++ Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
--- ghc --make -o eval Main.hs
+-- ghc --make -o lisp Main.hs
+-- Lisp>>> (load "stdlib.scm")
+-- Lisp>>> 
 
 import Evaluate
+import Environment
 import LispVal
 import LispError
 import Parse
@@ -14,11 +17,8 @@ import System.IO
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case length args of
-      0 -> runRepl
-      1 -> runOne $ args !! 0
-      _ -> putStrLn "Program takes 0 or 1 arguments"
+	args <- getArgs
+	if null args then runRepl else runOne $ args
 
 -- REPL
 flushStr :: String -> IO ()
@@ -40,8 +40,12 @@ until_ pred prompt action = do
       then return ()
       else action result >> until_ pred prompt action
 
-runOne :: String -> IO ()
-runOne expr = primitiveBindings >>= flip evalAndPrint expr
+-- Run the first argument and binds the rest to "args"
+runOne :: [String] -> IO ()
+runOne args = do
+    env <- primitiveBindings >>= flip bindVars [("args", List $ map String $ drop 1 args)] 
+    (runIOThrows $ liftM show $ eval env (List [Atom "load", String (args !! 0)])) 
+        >>= hPutStrLn stderr
 
 runRepl :: IO ()
 runRepl = primitiveBindings >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
